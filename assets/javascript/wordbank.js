a5afb7d98b5d88470de92e52dc6b4f6908aac0fa
Luke Stahl <Stahl1109@gmail.com>
2017-12-14 08:49:10 -0600

    Initial and Final Commit

---
+++ assets/javascript/wordbank.js
@@ -0,0 +1,64 @@
+Words = {};
+Words.List = [];
+
+// don't use uppercase letters in the word
+/*
+    Everyone likes templates:
+
+Words.List[0] = "";
+Words.List[1] = "";
+Words.List[2] = "";
+Words.List[3] = "";
+Words.List[4] = "";
+Words.List[5] = "";
+Words.List[6] = "";
+Words.List[7] = "";
+Words.List[8] = "";
+Words.List[9] = "";
+*/
+
+Words.List[0] = "lion";
+Words.List[1] = "cat";
+Words.List[2] = "dog";
+Words.List[3] = "light";
+Words.List[4] = "lamp";
+Words.List[5] = "love";
+Words.List[6] = "letter";
+Words.List[7] = "book";
+Words.List[8] = "cup";
+Words.List[9] = "sloth";
+
+Words.List[10] = "bear";
+Words.List[11] = "staple";
+Words.List[12] = "printer";
+Words.List[13] = "hat";
+Words.List[14] = "pen";
+Words.List[15] = "headphones";
+Words.List[16] = "chair";
+Words.List[17] = "paper";
+Words.List[18] = "drink";
+Words.List[19] = "eat";
+
+Words.List[20] = "shirt";
+Words.List[21] = "pants";
+Words.List[22] = "backpack";
+Words.List[23] = "flag";
+Words.List[24] = "clock";
+Words.List[25] = "coffee";
+Words.List[26] = "timer";
+Words.List[27] = "coat";
+Words.List[28] = "tie";
+Words.List[29] = "work";
+
+Words.List[30] = "locker";
+Words.List[31] = "stairs";
+Words.List[32] = "corridor";
+Words.List[33] = "hallway";
+Words.List[34] = "packers";
+Words.List[35] = "door";
+Words.List[36] = "video";
+Words.List[37] = "camera";
+Words.List[38] = "keyboard";
+Words.List[39] = "apple";
+
+// Words.Length = Words.List.length();
